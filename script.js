2af8aab5b8f68186855c589f8a389f02726d39df
Jai Gora <Jaigora24@gmail.com>
2021-03-24 23:07:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+//function that display value
 function dis(val) 
          { 
              document.getElementById("result").value+=val 
          }
-		 function solve() 
+//function that evaluates the digit and return result
+function solve() 
          { 
              let x = document.getElementById("result").value 
              let y = eval(x) 
              document.getElementById("result").value = y 
          }
-         function clr() 
+
+function clr() 
          { 
              document.getElementById("result").value = "" 
          } 
